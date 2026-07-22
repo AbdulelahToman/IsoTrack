1203699340ce673a803547c259d7782a1a4ce753
AbdulelahToman <toman.abdul@gmail.com>
2026-07-23 01:21:50 +0300

    Use SF Pro system typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Manrope } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const manrope = Manrope({
-  variable: "--font-manrope",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -32,7 +27,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${manrope.variable} ${manrope.className} ${geistMono.variable} antialiased`}
+        className={`${geistMono.variable} antialiased`}
       >
         {children}
       </body>
